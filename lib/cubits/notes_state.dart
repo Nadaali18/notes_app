6b41f12fba90fade6a2c52ccd2d974656ed44a7a
nadaali <nadaeloksh55@gmail.com>
2024-12-24 16:38:09 +0200

    create fetch notes methods

--- lib/cubits/notes_state.dart
+++ lib/cubits/notes_state.dart
@@ -5,10 +5,10 @@ abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
 class NotesLoading extends NotesState {}
-class NotesSucess extends NotesState {
+class NotesSuccess extends NotesState {
   final List<NoteModel> notes;
 
-  NotesSucess(this.notes);
+  NotesSuccess(this.notes);
 }
 class NotesFailure extends NotesState {
   final String errorMessage;
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit.dart';
 import 'package:notes_app/widgets/note_buttom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -7,20 +9,25 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      body:const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context, builder: (context){
-            return const AddNoteButtomSheet();
-          });
-        },
-        backgroundColor: Colors.purple,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return const AddNoteButtomSheet();
+                });
+          },
+          backgroundColor: Colors.purple,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          child: const Icon(Icons.add),
         ),
+      ),
     );
   }
 }
