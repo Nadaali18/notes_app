be112229c37e0313e5e3656838b5686c5e2082f9
nadaali <nadaeloksh55@gmail.com>
2024-12-24 16:27:16 +0200

    fix issues2

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -11,7 +11,9 @@ class NotesView extends StatelessWidget {
       body:const NotesViewBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          showModalBottomSheet(context: context, builder: (context){
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context, builder: (context){
             return const AddNoteButtomSheet();
           });
         },
